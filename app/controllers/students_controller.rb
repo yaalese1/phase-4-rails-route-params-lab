0094f71b000fa9bb72a4dd656a79832a74128e82
Yetunde Alese <yaalese1@gmail.com>
2023-01-10 18:15:16 -0500

    commit tree e5dd871704f5947123114740c6f0599fe856b81e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,34 @@
 class StudentsController < ApplicationController
-
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.where("first_name  LIKE :name or last_name LIKE :name", name:params[:name] ) 
+               else
+                 Student.all
+               end    
     render json: students
+  # def index
+  #   students = Student.all
+  #   include_json([
+  #         {  last_name: 'Johnson' }
+  #       ])
+  
+  #   render json: students
+  end
+#   students = if params[:name]
+#     Student.all.filter{|student| student.first_name == params[:name] or  student.last_name == params[:name]}
+#   else
+#     Student.all
+#   end    
+# render json: students
+
+  # def show
+  #   student = Students.find_by(params[:id])
+  #   if student render json: student
+  #   else null
+  #   end
+  def show 
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
